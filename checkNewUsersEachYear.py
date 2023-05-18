a563b47dd06957d7e15f6e5ff39b7a2d6e2865d3
怡杰 亢 <2055164440@qq.com>
2023-05-18 17:55:43 +0800

    每年新增用户数

---
+++ checkNewUsersEachYear.py
@@ -0,0 +1,25 @@
+from pyspark.sql import SparkSession
+from pyspark import find_spark_home, HiveContext
+from pyspark.sql.functions import year, col
+
+from SparkSessionBase import SparkSessionBase
+
+class TextRandJob(SparkSessionBase):
+    SPARK_URL = "local"
+    SPARK_APP_NAME = 'TextRandJob'
+    ENABLE_HIVE_SUPPORT = True
+
+    def __init__(self):
+        self.spark = self._create_spark_session()
+
+    def start(self):
+        hc=HiveContext(self.spark.sparkContext)
+        b_df=hc.table('users')
+        b_df.withColumn('year',year(col('user_yelping_since')))\
+            .groupBy('year')\
+            .count().show()
+
+# XXX 大数据分析代码
+
+if __name__ == '__main__':
+    TextRandJob().start()
