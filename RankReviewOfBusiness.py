70a6f5dd0d4a919e0b632a6a0a9ce821400064a4
怡杰 亢 <2055164440@qq.com>
2023-05-22 13:01:17 +0800

    按照年份排序，每个商户逐年评论数

--- RankReviewOfBusiness.py
+++ RankReviewOfBusiness.py
@@ -1,10 +1,10 @@
-from pyspark.sql import SparkSession
+from pyspark.sql import SparkSession, Window
 from pyspark import find_spark_home, HiveContext
 from pyspark.sql.functions import year, col, desc
 
 from SparkSessionBase import SparkSessionBase
 
-class TextRandJob(SparkSessionBase):
+class RankReviewOfBusiness(SparkSessionBase):
     SPARK_URL = "local"
     SPARK_APP_NAME = 'TextRandJob'
     ENABLE_HIVE_SUPPORT = True
@@ -15,11 +15,13 @@ class TextRandJob(SparkSessionBase):
     def start(self):
         hc=HiveContext(self.spark.sparkContext)
         #hc = HiveContext(sc)
+        hc = HiveContext(self.spark.sparkContext)
         re_df = hc.table('review')
-        result = re_df.groupby('rev_business_id').count().orderBy(desc('count'))
+        w1 = Window.partitionBy('rev_business_id').orderBy('year')
+        result = re_df.withColumn('year', year('rev_date')).groupBy('year', 'rev_business_id').count().withColumn('no.',dense_rank().over(w1))
         result.show()
 
 # XXX 大数据分析代码
 
 if __name__ == '__main__':
-    TextRandJob().start()
+    RankReviewOfBusiness().start()
