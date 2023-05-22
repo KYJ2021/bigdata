c6a3b96cf957a82f20d7947bcd2a7b00ca5b672f
怡杰 亢 <2055164440@qq.com>
2023-05-22 15:59:59 +0800

    本地spark查询并写入数据库（kyj，bigdata）

--- RankReviewOfBusiness.py
+++ RankReviewOfBusiness.py
@@ -1,11 +1,11 @@
 from pyspark.sql import SparkSession, Window
 from pyspark import find_spark_home, HiveContext
-from pyspark.sql.functions import year, col, desc
+from pyspark.sql.functions import year, col, desc, dense_rank
 
 from SparkSessionBase import SparkSessionBase
 
 class RankReviewOfBusiness(SparkSessionBase):
-    SPARK_URL = "local"
+    SPARK_URL = "local[8]"
     SPARK_APP_NAME = 'TextRandJob'
     ENABLE_HIVE_SUPPORT = True
 
@@ -17,9 +17,17 @@ class RankReviewOfBusiness(SparkSessionBase):
         #hc = HiveContext(sc)
         hc = HiveContext(self.spark.sparkContext)
         re_df = hc.table('review')
-        w1 = Window.partitionBy('rev_business_id').orderBy('year')
-        result = re_df.withColumn('year', year('rev_date')).groupBy('year', 'rev_business_id').count().withColumn('no.',dense_rank().over(w1))
-        result.show()
+        w1 = Window.partitionBy('rev_business_id').orderBy('which_year')
+        result = re_df.withColumn('which_year', year('rev_date')).groupBy('which_year', 'rev_business_id').count().withColumn('no.',dense_rank().over(w1))
+        # result.show()
+        result.write.format('jdbc') \
+            .option('url', 'jdbc:mysql://192.168.102.105:3306/bigdata') \
+            .option('driver', 'com.mysql.jdbc.Driver') \
+            .option('dbtable', 'business_review_each_year') \
+            .option('user', 'root') \
+            .option('password', 'admin') \
+            .mode('append') \
+            .save()
 
 # XXX 大数据分析代码
 
